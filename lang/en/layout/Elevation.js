5aff9b1e6df08be3e14982d42b3f70d9d5fbb8f7
lee <lh.man@aol.com>
2018-01-31 09:52:25 -0500

    Doc js to use new examples

--- lang/en/layout/Elevation.js
+++ lang/en/layout/Elevation.js
@@ -1,15 +1,19 @@
 export default {
   header: 'Elevation',
   headerText: 'The elevation helpers allow you to control relative depth, or distance, between two surfaces along the **z-axis**. There is a total of 25 elevation levels. You can set an element\'s elevation by using the class `elevation-{n}`, where `n` is a integer between 0-24 corresponding to the desired elevation.',
-  toc: [
-    {
-      text: 'Introduction',
-      href: 'introduction'
+  components: ['Elevation'],
+  examples: [{
+    default: {
+      header: 'Default Example',
+      desc: 'The default use of the elevation helper.'
     },
-    {
-      text: 'Playground',
-      href: 'playground'
+    textElevation: {
+      header: 'Text Elevation Example',
+      desc: 'The elevation helper using a v-card and text.'
+    },
+    imageElevation: {
+      header: 'Image Elevation Example',
+      desc: 'The elevation helper using a v-card and image.'
     }
-  ],
-  playgroundHeader: 'Playground'
+  }]
 }
