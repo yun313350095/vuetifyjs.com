c8121d41269670b7ced681782e57d9d1fe6fabee
lee <lh.man@aol.com>
2018-01-31 14:00:47 -0500

    fixes pr JL version

--- pages/layout/ElevationView.vue
+++ pages/layout/ElevationView.vue
@@ -30,7 +30,7 @@ export default {
   data() {
     return {
       selected: 0
-    };
+    }
   }
 };
 </script>
--- pages/layout/ImageView.vue
+++ pages/layout/ImageView.vue
@@ -31,7 +31,7 @@ export default {
   data() {
     return {
       selected: 0
-    };
+    }
   }
 };
 </script>
