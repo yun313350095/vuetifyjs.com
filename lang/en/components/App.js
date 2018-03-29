81aae5cf0dc270a15d4250c3337b8377003d3d82
Sean Kimball <sean@vuetifyjs.com>
2018-03-29 15:59:15 -0700

    Add VApp to lang

---
+++ lang/en/components/App.js
@@ -0,0 +1,6 @@
+export default {
+  components: ['v-app'],
+  props: {
+    dark: 'Applies the dark theme variant'
+  }
+}
--- lang/en/components/index.js
+++ lang/en/components/index.js
@@ -1,3 +1,4 @@
+import App from './App'
 import Alerts from './Alerts'
 import ApiExplorer from './ApiExplorer'
 import Avatars from './Avatars'
@@ -41,6 +42,7 @@ import Toolbars from './Toolbars'
 import Tooltips from './Tooltips'
 
 export default {
+  App,
   Alerts,
   ApiExplorer,
   Avatars,
